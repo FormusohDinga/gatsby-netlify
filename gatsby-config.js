d28d4f3cffcb71a43f309ff98dab24e87f619aef
Ronald Formusoh <rformusoh1@gmail.com>
2020-01-17 15:56:36 -0600

    First commit

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+module.exports = {
+	siteMetadata: {
+		title: `Base of Operation`,
+		author: `Ronald Formusoh`,
+		description: `s`,
+		siteUrl: ``,
+		social: {
+			twitter: ``,
+		},
+	},
+	plugins: [
+		{
+			resolve: `gatsby-plugin-material-ui`,
+		},
+		{
+			resolve: `gatsby-plugin-netlify-cms`,
+		},
+	],
+}
